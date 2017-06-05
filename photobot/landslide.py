1725c6db4e89d3f87dc835795a2436790489db54
Karsten Wolf <karstenwo@web.de>
2017-06-05 19:22:27 +0200

    Rewrite.

--- photobot/landslide.py
+++ photobot/landslide.py
@@ -1,3 +1,16 @@
+kwdbg = True
+
+if 0: #not kwdbg:
+    size(0, 0)
+    if not WIDTH and HEIGHT:
+        size(1024, 768)
+else:
+    size(1024, 768)
+
+
+RATIO = WIDTH / HEIGHT
+
+background( 0.333 )
 
 import sys, os
 
@@ -7,114 +20,203 @@ pp = pprint.pprint
 # need a different name
 import random as rnd
 
-background( 0.5 )
+if kwdbg:
+    # make random choices repeatable for debugging
+    rnd.seed(0)
 
+# import photobot
 try:
     pb = ximport("photobot")
 except ImportError:
     pb = ximport("__init__")
     reload(pb)
 
+# import extensions if nodebox version < 1.9.18
+try:
+    imagefiles
+except NameError:
+    from nodeboxExtensions import *
 
-c = pb.canvas(800, 600)
+def cropImageToRatioHorizontal( layer, ratio ):
+    w, h = layer.bounds()
+    neww = int( round( h*ratio) )
+    d = int( neww / 2.0 )
+    x,y,w,h = insetRect( (0,0,w,h), d, 0 )
+    layer.img = layer.img.crop(box=(x,y,x+w,y+h))
+    return layer
+    
 
-folder = "./images"
-#folder = "/Library/Desktop Pictures/wallstreet_wallpapers/Objects"
+def insetRect( rectangle, horInset, vertInset):
+    x, y, w, h = rectangle
+    dh = horInset / 2.0
+    dv = vertInset / 2.0
+    return x+dh, y+dv, w-horInset, h-vertInset
 
-# finally an image folder that should exist on most macs
-folder = "/Library/Desktop Pictures"
+def aspectRatio(size, maxheight):
+    """scale a (w,h) tuple to maxheight."""
+    
+    # NEEDED: scale to center square, not height
+    
+    w, h = size
+    if h == maxheight:
+        return 1.0
+    elif maxheight == 0:
+        return 1.0
+    else:
+        return float(maxheight) / h
 
-rawfiles = [f for f in os.listdir(folder)]
-# rawfiles = files( folder + "/*.jpg" )
 
+
+
+# create the canvas
+c = pb.canvas(int(WIDTH), int(HEIGHT))
+
+
+# get all images from system "Desktop Pictures" folder
+filetuples = imagefiles( "/Library/Desktop Pictures", False )
+
+# filter out all 1 pix one color images by ignoring all files < 100k
 tiles = []
-for t in rawfiles:
-    if os.path.splitext(t)[1].lower() in [".jpg", ".png", ".tif", ".tiff", ".jpeg", ".gif"]:
-        path = t
-        if not path.startswith('/'):
-            path = os.path.abspath(os.path.join(folder, t))
-        tiles.append( path )
-# pp(tiles)
+for t in filetuples:
+    path, filesize, lastmodified, mode, islink = t
+    if filesize < 100000:
+        continue
+    tiles.append( path )
+
+
+rnd.shuffle(tiles)
+
+
+
+# CONFIGURATION
+
+columns = 3
+rows = 2
+
+randomblur = False
+paintoverlay = False
+
+
 # 
-columns = 4
-rows = 4
+y_offset = HEIGHT / (rows)
+
 
 # 
-y_offset = HEIGHT / (rows+1)
-
-# x_offset = WIDTH / (columns * 2)
-
-enoughTiles = len(tiles) > (columns*2*rows)
-
-picts = []
-for t in range(columns*rows*2):
-    s = choice(tiles)
-    picts.append(s)
-    if enoughTiles:
-        tiles.remove(s)
-
-rnd.shuffle(picts)
-nextpictpath = picts.pop()
-c.layer(nextpictpath)
-c.layers[1].scale(0.2,0.2)
+if 1:
+    top = c.layer(tiles.pop())
+    w, h = c.layers[ top ].bounds()
+    s = aspectRatio( (w,h), HEIGHT )
+    c.layers[1].scale(s, s)
 
 
-def layerTop( c ):
+def topLayer( c ):
     # return top layer index
-    nlayers = len(c.layers)
-    return nlayers -1
+    return len(c.layers) -1
 
 
 for j in range(rows):
+    colw = 0
     for i in range(columns):
 
         # create image in canvas at 0,0
-        nextpictpath = picts.pop()
-        print repr(nextpictpath)
+        nextpictpath = tiles.pop()
 
-        c.layer( nextpictpath )
-
-        nlayers = len(c.layers)
-        lastIndex = nlayers -1
-        c.layers[lastIndex].scale(0.2,0.2)
-
-        # add contrast
-        c.layers[ lastIndex ].contrast(1.1)
+        # top is the index of the image layer
+        top = c.layer( nextpictpath )
 
         # get current image bounds
-        w, h = c.layers[ lastIndex ].bounds()
+        w, h = c.layers[ top ].bounds()
 
-        # create gradient layer
-        c.gradient(pb.LINEAR, w/2, h)
+        # calculate scale & apply
+        s = aspectRatio( (w,h), y_offset)
+        c.layers[ top ].scale(s, s)
 
-        nlayers = len(c.layers)
-        lastIndex = layerTop( c )
+        # uniform
+        layer = c.layers[ top ]
+        layer = cropImageToRatioHorizontal( layer, RATIO )
+        c.layers[ top ]= layer
 
-        # layer + 4 flip
-        c.layers[ lastIndex ].flip()
+        # get the new image bounds
+        w, h = c.layers[ top ].bounds()
 
-        # layer +4 translate half a pict right
-        c.layers[ lastIndex ].translate(w/2, j*y_offset)
+        # add contrast
+        c.layers[ top ].contrast(1.1)
 
-        # create gradient layer
-        c.gradient(pb.LINEAR, w/2, h)
-        lastIndex = layerTop( c )
 
-        # merge gradient with 
-        c.merge([ lastIndex - 1 , lastIndex ])
-        lastIndex = layerTop( c )
+        r = 0.4 #rnd.random()
+        # 10%
+        if r < 0.1:
+            print "COSINE/LINEAR"
+            # create gradient layer
+            # top is now gradient index
+            top = c.gradient(pb.LINEAR, w/2, h)
+            c.layers[ top ].flip( pb.HORIZONTAL )
 
-        c.layers[ lastIndex ].brightness(1.4)
+            # layer + 4 flip
+            # c.layers[ top ].flip( pb.HORIZONTAL )
+
+            # layer +4 translate half a pict right
+            c.layers[ top ].translate(w/2, j*y_offset)
+
+            # create gradient layer
+            # top is now second gradient index
+            top = c.gradient(pb.LINEAR, w/2, h)
+            # merge both gradients; destroys top layer
+            c.merge([ top-1 , top ])
+        elif 0.1 <= r < 0.5:
+            print "SINE"
+            # create gradient layer
+            # top i now gradient index
+            top = c.gradient(pb.SINE, w, h)
+            
+        elif 0.6 <= r < 0.75:
+            print "RADIALCOSINE"
+            # create gradient layer
+            # top i now gradient index
+            top = c.gradient(pb.RADIALCOSINE, w, h)
+            c.layers[top].invert()
+        else:
+            print "ROUNDRECT"
+            # 25%
+            top = c.gradient(pb.ROUNDRECT, w, h, radius=int(w/5.0))
+            
         
-        c.layers[ lastIndex ].mask()
-        lastIndex = layerTop( c )
+        top = topLayer( c )
 
-        c.layers[ lastIndex ].translate(i*w/3, j*y_offset)
+        c.layers[ top ].brightness(1.4)
+        # mask destroys top layer
+        c.layers[ top ].mask()
 
-        if 0: #random() > 0.5:
-            c.layers[ lastIndex ].flip()
+        # top layer is now image with mask
+        top = topLayer( c )
+        # c.layers[ top ].translate(colw+i*w/3, j*y_offset)
+        c.layers[ top ].translate(colw+i*w, j*y_offset)
+        # colw += i*w/2.0
 
-        if 0: #random() > 0.5:
-            c.layers[ lastIndex ].blur()
+        if randomblur:
+            if rnd.random() > 0.5:
+                c.layers[ top ].flip()
 
-c.draw(10, 10)
+            if rnd.random() > 0.5:
+                c.layers[ top ].blur()
+
+if 1:
+    # orange hue mask finish
+    top = c.fill((200,100,0))
+    c.layers[top].opacity(30)
+    c.layers[top].hue()
+
+if paintoverlay:
+    # paint overlay
+    top = c.layer( os.path.abspath("./paint.jpg") )
+    w, h = c.layers[top].bounds()
+    xs = WIDTH / float(w)
+    ys = HEIGHT / float(h)
+    s = max(xs,ys)
+    c.layers[top].scale(s, s)
+    c.layers[top].opacity(10)
+    c.layers[top].overlay()
+
+
+
+c.draw(0, 0)
