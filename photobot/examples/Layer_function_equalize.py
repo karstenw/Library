f39e39f4e92797c6c2e553913590057f000a6102
Karsten Wolf <karstenwo@web.de>
2022-03-20 21:30:09 +0100

    Imagewells update.

--- photobot/examples/Layer_function_equalize.py
+++ photobot/examples/Layer_function_equalize.py
@@ -43,7 +43,7 @@ rnd.shuffle(tiles)
 img1path = tiles.pop()
 img2path = tiles.pop()
 
-# create a gray canvas
+# create a white canvas
 c = pb.canvas( WIDTH, HEIGHT)
 c.fill( (192, 192, 192) )
 
@@ -53,43 +53,30 @@ c.fill( (192, 192, 192) )
 
 #  create, scale and place the image
 x, y = 10, 10
-top, w1, h1 = pb.placeImage(c, img1path, x, y, tilewidth, "Image 1")
-pb.label(c, "Image 1", x, y)
-
-#
-# Image 2
-#
-x, y = w1+20, 10
-top, w2, h2 = pb.placeImage(c, img2path, x, y, tilewidth, "Image 2")
-pb.label(c, "Image 2", x, y)
 
 
 #
-# Subtract Images 1 & 2
+# Normal Image 1
 #
 
-h = max(h1, h2)
+h = 10
 x, y = 10 , h + 20
 
-top, w3, h3 = pb.placeImage(c, img1path, x, y, fullwidth, "Image 3")
-top, w4, h4 = pb.placeImage(c, img2path, x, y, fullwidth, "Image 4")
-# c.layers[top].subtract()
-c.top.subtract()
+top, w, h = pb.placeImage(c, img1path, x, y, fullwidth, "Image 1", 0)
 
-pb.label(c, "Subtract Image 1 over Image 2", x, y)
+pb.label(c, "Normal Image 1", x, y)
 
 
 #
-# Subtract Images 2 & 1
+# Equalize Image 1
 #
 
-h = max(h3, h4)
 x, y = 10 , h + 20 + y
 
-top, w4, h4 = pb.placeImage(c, img2path, x, y, fullwidth, "Image 5")
-top, w3, h3 = pb.placeImage(c, img1path, x, y, fullwidth, "Image 6")
-c.top.subtract()
-pb.label(c, "Subtract Image 2 over Image 1", x, y)
+top, w4, h4 = pb.placeImage(c, img1path, x, y, fullwidth, "Image 2", 1)
+c.top.equalize()
+pb.label(c, "Equalized Image 1", x, y)
 
 # draw the result
-c.draw(name="Layer_function_subtract")
+c.draw(name="Layer_function_equalize")
+
