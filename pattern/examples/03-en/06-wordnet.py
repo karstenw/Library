5197709c7effd7ff1391b79c2f4d7adc21eca712
Karsten Wolf <karstenwo@web.de>
2022-09-20 17:27:38 +0200

    Update 06-wordnet.py

--- pattern/examples/03-en/06-wordnet.py
+++ pattern/examples/03-en/06-wordnet.py
@@ -5,11 +5,14 @@ from builtins import str, bytes, dict, int
 
 import os
 import sys
-# sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", ".."))
-pattern = ximport("pattern")
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+# pattern = ximport("pattern")
 
 from pattern.en import wordnet
 from pattern.en import NOUN, VERB
+
+# import pdb
+# pdb.set_trace()
 
 # WordNet is a lexical database for the English language.
 # It groups English words into sets of synonyms called synsets, provides short, general definitions,
