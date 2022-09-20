0a4b31f133a00ad14edf6185667640411ee87e85
Karsten Wolf <karstenwo@web.de>
2022-09-20 17:27:22 +0200

    Removed "web" library - too ancient

--- web/_web_example8-2.py
+++
@@ -1,7 +0,0 @@
-# Definitions from the Urban Dictionary.
-
-web = ximport("__init__")
-reload(web)
-
-definitions = web.urbandictionary.search("human")
-text( choice(definitions), 50, 50, width=300)
