e936a4a02c061323b87ebb541c6a1dd4463855ba
Karsten Wolf <karstenwo@web.de>
2023-05-11 12:43:13 +0200

    Bigger canvas

--- perception/perception_example1.py
+++ perception/perception_example1.py
@@ -11,20 +11,20 @@ def load(node):
     
     global g
     # Load a new cluster of nodes surrounding the clicked node.
-    # Note the "max" parameter - you won't find it documented.
+    # Note the "maxedges" parameter - you won't find it documented.
     # We use it here to cap the number of rules returned.
     # A fast graph is more important right now than all of the data.
-    g = perception.cluster(str(node), depth=2, max=30, labeled=False)
-    g.distance = 1.4
+    g = perception.cluster(str(node), depth=2, maxedges=30, labeled=1)
+    g.distance = 2.8
     g.layout.n = 2000
     g.styles.apply()
-    g.layout.force = 0.005 # lower if nodes are twitching
+    g.layout.force = 0.001 # lower if nodes are twitching
     g.events.click = load
 
 load("cool")
 
-size(600, 600)
-speed(30)
+size(850, 850)
+speed(60)
 def draw():
     g.update()
     g.draw(traffic=True, directed=True)
