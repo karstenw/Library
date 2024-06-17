f5124868c45ebf8e1d934c8d8b867629ae43eb26
Karsten Wolf <karstenwo@web.de>
2024-06-17 14:11:50 +0200

    Animation sample added.

---
+++ flowerewolf/flowerewolf_example3.py
@@ -0,0 +1,29 @@
+# Note: you'll also need the NodeBox English Linguistics library:
+# old: http://nodebox.net/code/index.php/Linguistics
+# new: https://github.com/karstenw/linguistics
+
+from random import seed, sample
+
+flowerewolf = ximport("flowerewolf")
+
+size( 1000, 900 )
+fontsize(32)
+
+# select 13 random fonts
+
+allnouns = list(flowerewolf.allnouns)
+
+speed( 0.334 )
+
+def setup():
+    pass
+
+def draw():
+    fonts = sample( fontnames(), 20 )
+    topic = choice( allnouns )
+    print("topic:", topic)
+    flowerewolf.dada(
+            str(topic), 
+            foreground=color(1,1,1,0.9),
+            background=color(1,0,0), 
+            fonts=fonts)
