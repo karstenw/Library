7d2db96c78d6339d865124d4f1fabd72aa018361
Karsten Wolf <karstenwo@web.de>
2025-08-27 12:16:55 +0200

    Deleted py2 stuff

--- graph/__init__.py
+++ graph/__init__.py
@@ -36,18 +36,11 @@ from . import proximity
 from . import style
 
 # py3 stuff
-py3 = False
-try:
-    unicode('')
-    punicode = unicode
-    pstr = str
-    punichr = unichr
-except NameError:
-    punicode = str
-    pstr = bytes
-    py3 = True
-    punichr = chr
-    long = int
+punicode = str
+pstr = bytes
+py3 = True
+punichr = chr
+long = int
 
 
 def sortlist(thelist, thecompare):
