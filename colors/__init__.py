984d0afe888a04a8ccdf87bf52ae091f661aac65
Karsten Wolf <karstenwo@web.de>
2025-12-06 12:39:25 +0100

    Update __init__.py

--- colors/__init__.py
+++ colors/__init__.py
@@ -52,6 +52,8 @@ from copy import deepcopy
 from xml.dom.minidom import parseString
 from random import random, choice
 
+from functools import cmp_to_key
+
 try:
     # NodeBox / Cocoa specific functionality.
     # Our library can still do a lot of interesting stuff without these!
@@ -82,25 +84,6 @@ except NameError:
     punichr = chr
     long = int
     xrange = range
-
-def cmp_to_key(mycmp):
-    'Convert a cmp= function into a key= function'
-    class K:
-        def __init__(self, obj, *args):
-            self.obj = obj
-        def __lt__(self, other):
-            return mycmp(self.obj, other.obj) < 0
-        def __gt__(self, other):
-            return mycmp(self.obj, other.obj) > 0
-        def __eq__(self, other):
-            return mycmp(self.obj, other.obj) == 0
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
-    return K
 
 def sortlistfunction(thelist, thecompare):
     if py3:
