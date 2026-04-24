f4d3567d87554db4029aba34ff5a6f2e1a55eee5
Karsten Wolf <karstenwo@web.de>
2026-04-24 14:21:09 +0200

    Update Layer_colorize.py

--- photobot/examples/Layer_colorize.py
+++ photobot/examples/Layer_colorize.py
@@ -65,7 +65,7 @@ c.fill( (192, 192, 192) )
 # Image 1
 #
 
-print( img1path.encode("utf-8") )
+print( img1path )
 #  create, scale and place the image
 x, y = 10, 10
 top, w1, h1 = pb.placeImage(c, img1path, x, y, fullwidth, "Image 1")
