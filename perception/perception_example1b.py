a8a4897df3d117899aa32a3b89a3856121b49ff5
Karsten Wolf <karstenwo@web.de>
2026-04-28 16:25:11 +0200

    Update perception_example1b.py

--- perception/perception_example1b.py
+++ perception/perception_example1b.py
@@ -30,7 +30,7 @@ def load( theConcept, lang="" ):
     # Note the "maxedges" parameter - you won't find it documented.
     # We use it here to cap the number of rules returned.
     # A fast graph is more important right now than all of the data.
-    g = perception.cluster( str(theConcept), depth=2, maxedges=140, labeled=1, lang=lang)
+    g = perception.cluster( str(theConcept), depth=2, maxedges=100, labeled=1, lang=lang)
     g.distance = 3.1
     g.layout.n = 2000
     g.styles.apply()
