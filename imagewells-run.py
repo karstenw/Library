7893ae31cea1ce966bca4ba0d17ad15b7b0875ce
Karsten Wolf <karstenwo@web.de>
2026-05-06 18:35:04 +0200

    Update imagewells-run.py

--- imagewells-run.py
+++ imagewells-run.py
@@ -2,9 +2,7 @@ import pprint
 pp=pprint.pprint
 
 import imagewells
-loadImageWell = imagewells.loadImageWell
-
-imagewell = loadImageWell()
+imagewell = imagewells.loadImageWell()
 
 for key in imagewell:
     print("  %s:   %i" %(key, len(imagewell[key])))
