3f48ec4524f1ce732aab2adf79e7119466a82d12
Karsten Wolf <karstenwo@web.de>
2026-05-08 23:43:55 +0200

    Update colors_example10.py

--- colors/colors_example10.py
+++ colors/colors_example10.py
@@ -1,8 +1,8 @@
-size(1100, 850)
+size(1200, 850)
 background(0.15, 0.15, 0.7)
 colors = ximport("colors")
 
 font("Georgia", 292)
-p = textpath("gradient", 10, 350)
+p = textpath("gradient", 50, 500)
 g = colors.gradient(color(1, 1, 0.0), color(0.35, 0.35, 0.0))
 colors.outline(p, g)
